51f83ee84c09834bcf7c2a84986c38a228df0fb1
maciekzientarski1234-jpg <maciekzientarski1234@gmail.com>
2026-03-15 14:34:51 +0100

    add

--- test_data/registration_data_generator.py
+++ test_data/registration_data_generator.py
@@ -1,15 +1,27 @@
-from faker import Faker
-from utils.custom_types import Gender
-import random
+from tests.base_test import BaseTest
+from time import sleep
+from test_data.registration_data_generator import RegistrationDataGenerator
 
-class RegistrationDataGenerator:
-    def __init__(self):
-        self.GENDER= random.choice([Gender.MALE, Gender.FEMALE])
-        self.__fake = Faker("pl_PL")
-        if self.GENDER == Gender.FEMALE:
-            self.FIRST_NAME = self.__fake.first_name_female()
-        else:
-            self.FIRST_NAME = self.__fake.first_name_male()
-        self.EMAIL = self.__fake.email()
-        self.PASSWORD = self.__fake.password()
-        self.DATE_OF_BIRTH = self.__fake.date_of_birth()
+class RegistrationTest(BaseTest):
+    def setUp(self):
+        super().setUp()
+        # Obiekt data ma mieć w sobie dane testowe
+        self.data = RegistrationDataGenerator()
+        self.authentication_page = self.home_page.click_sign_in()
+        self.authentication_page.enter_create_account_email(self.data.EMAIL)
+        self.create_account_page = self.authentication_page.click_create_account()
+
+
+    def testNoLastname(self):
+        self.create_account_page.choose_gender(self.data.GENDER)
+        self.create_account_page.enter_first_name(self.data.FIRST_NAME)
+        self.assertEqual(self.data.EMAIL, self.create_account_page.get_entered_email())
+        self.create_account_page.enter_password(self.data.PASSWORD)
+        self.create_account_page.select_date_of_birth(self.data.DATE_OF_BIRTH)
+        self.create_account_page.click_register_button()
+        expected_number_of_errors_message = "There is 1 error"
+        actual_number_of_errors_message = self.create_account_page.get_number_of_errors_message()
+        self.assertEqual(expected_number_of_errors_message, actual_number_of_errors_message)
+        visible_errors = self.create_account_page.get_visible_errors()
+        expected_errors = ["lastname is required."]
+        self.assertCountEqual(expected_errors, visible_errors)
