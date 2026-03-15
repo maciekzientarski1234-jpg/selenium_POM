05f3a02d895f9846fc432067096d00acd00804ff
maciekzientarski1234-jpg <maciekzientarski1234@gmail.com>
2026-03-15 13:13:34 +0100

    add

--- pages/create_account_page.py
+++ pages/create_account_page.py
@@ -1,12 +1,22 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from pages.base_page import BasePage
 from time import sleep
+from utils.custom_types import Gender
 
 class Locators:
     """
     CreateAccountPage locators
     """
     FIRST_NAME = (By.ID, "customer_firstname")
+    GENDER_MALE = (By.XPATH, '//label[@for="id_gender1"]')
+    GENDER_FEMALE = (By.XPATH, '//label[@for="id_gender2"]')
+    EMAIL = (By.ID, 'email')
+    PASSWORD = (By.ID, 'passwd')
+    REGISTER_BTN = (By.ID, 'submitAccount')
 
 
 class CreateAccountPage(BasePage):
@@ -14,11 +24,35 @@ class CreateAccountPage(BasePage):
     Create Account Page Object
     """
     def choose_gender(self, gender):
-        pass
+        """
+        Choose Mr or Mrs
+        """
+        if gender == Gender.MALE:
+            self.driver.find_element(*Locators.GENDER_MALE).click()
+        else:
+            self.driver.find_element(*Locators.GENDER_FEMALE).click()
 
     def enter_first_name(self, first_name):
+        """
+        Enter First Name
+        """
         self.driver.find_element(*Locators.FIRST_NAME).send_keys(first_name)
 
+    def enter_password(self, password):
+        """
+        Enter Password
+        """
+        self.driver.find_element(*Locators.PASSWORD).send_keys(password)
+
+    def get_entered_email(self):
+        """
+        Get Email entered on previous page
+        """
+        return self.driver.find_element(*Locators.EMAIL).get_attribute("value")
+
+
+
+
     def _verify_page(self):
-        # TODO: Improve this mechanism!!!
-        sleep(3)
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(Locators.FIRST_NAME))
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(Locators.REGISTER_BTN))
--- tests/registration_test.py
+++ tests/registration_test.py
@@ -1,13 +1,20 @@
 from tests.base_test import BaseTest
 from time import sleep
+from test_data.registration_data_generator import RegistrationDataGenerator
 
 class RegistrationTest(BaseTest):
     def setUp(self):
         super().setUp()
+        # Obiekt data ma mieć w sobie dane testowe
+        self.data = RegistrationDataGenerator()
         self.authentication_page = self.home_page.click_sign_in()
-        self.authentication_page.enter_create_account_email("jshjhd@gmail.com")
+        self.authentication_page.enter_create_account_email(self.data.EMAIL)
         self.create_account_page = self.authentication_page.click_create_account()
 
+
     def testNoLastname(self):
-        self.create_account_page.enter_first_name("Marcin")
-        sleep(3)
+        self.create_account_page.choose_gender(self.data.GENDER)
+        self.create_account_page.enter_first_name(self.data.FIRST_NAME)
+        self.assertEqual(self.data.EMAIL, self.create_account_page.get_entered_email())
+        self.create_account_page.enter_password(self.data.PASSWORD)
+        sleep(2)
